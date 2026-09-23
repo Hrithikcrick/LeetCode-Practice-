99ed600e4205525c5ee6764dd3a35237064882eb
Hrithik Sharma <90860334+Hrithikcrick@users.noreply.github.com>
2026-09-23 11:12:49 +0530

    Time: 1750 ms (24.71%), Space: 34.8 MB (37.46%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,20 +1,22 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
+    def canPartition(self, nums: list[int]) -> bool:
         n=len(nums)
-        total=sum(nums)
-        if total%2!=0:
-            return False
-        target=total//2
-        dp=[[False for _ in range(target+1)] for _ in range(n+1)]
-        for i in range(n+1):
-            dp[i][0]=True
-        for i in range(1,n+1):
-            for s in range(1,target+1):
-                not_take=dp[i-1][s]
-                take = False
-                if nums[i-1]<=s:
-                    take=dp[i-1][s-nums[i-1]]
-                dp[i][s]=take or not_take
-        return dp[n][target]
         
+        total=sum(nums)
+        if total%2==1:
+            return False
+        target=sum(nums)//2
+        dp=[[False]*(target+1) for _ in range(n)]
+        for i in range(n):
+            dp[i][0]=True
+        if nums[0]<=target:
+            dp[0][nums[0]]=True
+        for i in range(1,n):
+            for t in range(1,target+1):
+                not_take=dp[i-1][t]
+                take=False
+                if nums[i]<=t:
+                    take=dp[i-1][t-nums[i]]
+                dp[i][t]=take or not_take
+        return dp[n-1][target]
         
